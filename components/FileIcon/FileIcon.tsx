f21c9faabff13c5791a05849ee8312c691b1759c
David Smith <dnsmith124@gmail.com>
2023-06-29 10:46:57 -0500

    address font awesome icons flashing unstyled on load

--- components/FileIcon/FileIcon.tsx
+++ components/FileIcon/FileIcon.tsx
@@ -15,15 +15,15 @@ const FileIcon: FC<FileIconProps> = ({ subIcon = null, label = '', onClick = () 
 
   return(
     <button className={`cursor-pointer text-center mx-5 mb-5  hover:underline ${active && 'underline'}`} onClick={onClick}>
-      <div className='relative'>
-        <FontAwesomeIcon className='w-20 h-20 mb-2 drop-shadow-md' icon={faFile} />
+      <div className='relative flex justify-center'>
+        <FontAwesomeIcon width="80px" style={{display: 'inline-block'}} className='w-20 h-20 drop-shadow-md' icon={faFile} />
         {subIcon !== null &&
-          <div className='absolute top-0 right-0 bottom-0 left-0 z-10 flex justify-center items-center'>
-            <FontAwesomeIcon className='w-9 h-9 text-darkerGray fill-current' icon={subIcon} />
+          <div className='absolute top-3 right-0 bottom-0 left-0 z-10 flex justify-center items-center'>
+            <FontAwesomeIcon width="36px" style={{height: '36px'}} className=' text-darkerGray fill-current' icon={subIcon} />
           </div>
         }
       </div>
-      <p className={`${source_code_pro.className} text-sm`} >{label}</p>
+      <p className={`${source_code_pro.className} text-sm mt-2`} >{label}</p>
     </button>
   )
 }
